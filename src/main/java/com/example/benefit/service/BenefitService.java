24860f5e730355e7655f5ab5b5a69a3bdd735ae0
Nika Avalishvili <nika.avalishvili@solveva.com>
2022-11-28 11:19:47 +0400

    Implement RabbitMQ and update tests

--- src/main/java/com/example/benefit/service/BenefitService.java
+++ src/main/java/com/example/benefit/service/BenefitService.java
@@ -3,24 +3,28 @@ package com.example.benefit.service;
 import com.example.benefit.exceptionHandler.BenefitNotFoundException;
 import com.example.benefit.model.Benefit;
 import com.example.benefit.model.BenefitDTO;
+import com.example.benefit.model.BenefitDTOForRabbitMQ;
 import com.example.benefit.model.BenefitMapper;
 import com.example.benefit.repository.BenefitRepository;
 import com.example.benefit.repository.BenefitTypeRepository;
 import com.example.benefit.repository.CalculationMethodRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class BenefitService {
 
     private final BenefitRepository benefitRepository;
     private final BenefitMapper benefitMapper;
-
     private final BenefitTypeRepository benefitTypeRepository;
     private final CalculationMethodRepository calculationMethodRepository;
+    private final StreamBridge streamBridge;
 
     public BenefitDTO createAndUpdateBenefit(BenefitDTO benefitDTO) {
         Benefit benefit = benefitMapper.dtoToEntity(benefitDTO);
@@ -35,10 +39,19 @@ public class BenefitService {
             benefit.setCalculationMethod(calculationMethodRepository.findByName(calculationMethodName));
         }
 
+        BenefitDTOForRabbitMQ benefitDTOForRabbitMQ = BenefitDTOForRabbitMQ.builder()
+                .id(benefitDTO.getId())
+                .name(benefitDTO.getName())
+                .benefitTypeName(benefitDTO.getBenefitTypeDTO().getName())
+                .calculationMethodName(benefitDTO.getCalculationMethodDTO().getName())
+                .build();
+
         if (benefitRepository.findByName(benefit.getName()) == null) {
             benefitRepository.save(benefit);
+            streamBridge.send("benefit-out-0",benefitDTOForRabbitMQ);
             return benefitMapper.entityToDto(benefit);
         } else {
+            streamBridge.send("benefit-out-0",benefitDTOForRabbitMQ);
             return benefitMapper.entityToDto(benefitRepository.findByName(benefit.getName()));
         }
     }
